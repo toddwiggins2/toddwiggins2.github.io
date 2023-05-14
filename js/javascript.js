5cd0cfd9bd0b79b0cfc05ba12316c08165e8c1b4
toddwiggins2 <toddwiggins2@gmail.com>
2023-05-14 13:39:32 -0400

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -9,7 +9,6 @@ function githubFunction() {
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     }
