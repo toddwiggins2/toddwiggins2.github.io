8a137ce4381af69a27db24f21fb1cb7708563092
toddwiggins2 <toddwiggins2@gmail.com>
2023-05-23 17:58:31 -0400

    updates

--- js/javascript.js
+++ js/javascript.js
@@ -15,5 +15,22 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenElements = document.querySelectorAll(".slide-in-hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+let prevScrollPos = window.pageYOffset;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // Scrolling up
+    header.classList.remove('hidden');
+  } else {
+    // Scrolling down
+    header.classList.add('hidden');
+  }
+
+  prevScrollPos = currentScrollPos;
+});
